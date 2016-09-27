4141599738a62112bad0a6b2b930511ea5bc884b
jhorowitz16 <joshua.horowitz@berkeley.edu>
2016-09-27 04:13:16 -0700

    wrote the stack logic, but tested with plain numbers

--- Cell.java
+++ Cell.java
@@ -2,7 +2,10 @@ import java.util.*;
 
 public class Cell {
 
-    private boolean literal;
+    private boolean literal; // literal means just a number
+    // figure out a good definition of literal
+    // literal as in 3, or literal as in 4/3
+    // make sure variables set corretly with constructor TODO
     private String strExpr;
     private double val;
     private ArrayList<String> dependencies; // we want easy resizing of this
@@ -13,12 +16,22 @@ public class Cell {
         this.literal = true;
         this.dependencies = new ArrayList<String>();
         String[] words = strExpr.split(" ");
+
+        if (words.length == 1) { 
+            // just a number
+            this.val = Double.parseDouble(words[0]); 
+            return;
+        }
+
         for (int i=0; i<words.length; i++) {
             char c = words[i].charAt(0);
             if ( c >= (int)'A' && c <= (int)'Z' ) {
                 // there's a dependency!
                 dependencies.add(words[i]);
+                this.literal = false;
             }
+            else if (isOperator(c))
+                this.literal = false; // not just a number then...
         }
     }
 
@@ -27,7 +40,7 @@ public class Cell {
     public boolean isLiteral() {
         return this.literal;
     }
-    public String strExpr() {
+    public String getStrExpr() {
         return this.strExpr;
     }
     public double getVal() {
@@ -58,5 +71,12 @@ public class Cell {
         if (literal)
             return String.format("%.5f", this.val);
         return strExpr; // TODO should always return the string
+    }
+
+    // utils
+    public static boolean isOperator(char s) {
+        if (s == '+' || s == '-' || s == '*' || s == '/')
+            return true;    
+        return false;
     }
 }
--- Spreadsheet.java
+++ Spreadsheet.java
@@ -17,12 +17,20 @@ public class Spreadsheet{
         return true;
     }
 
-
-    // found on stackoverflow - for converting "row 0" to row A
-    private static String getCharForNumber(int i) {
-        return i > 0 && i < 27 ? String.valueOf((char)(i + 64)) : null;
+    public static boolean isOperator(String s) {
+        if (s == "+" || s == "-" || s == "*" || s == "/")
+            return true;    
+        return false;
     }
 
+
+    // using ASCII representation for converting "row 0" to row A
+    private static String getStrForNumber(int i) {
+        return String.valueOf((char)(i + 65));
+    }
+
+    // HashMap for mappings between Cells and their location strings
+    public static HashMap<String, Cell> locationMap = new HashMap<String, Cell>();
 
     // return a 2d array of cells based on the input
     public static Cell[][] readInput() {
@@ -36,8 +44,8 @@ public class Spreadsheet{
         int rows = -1;
         int cols = -1;
         try {
-            rows = Integer.parseInt(params[0]); 
-            cols = Integer.parseInt(params[1]); 
+            cols = Integer.parseInt(params[0]); 
+            rows = Integer.parseInt(params[1]); 
         } catch (NumberFormatException e) {
             System.out.println("Error: spreadsheet dimensions must be two integers");
         }
@@ -52,11 +60,16 @@ public class Spreadsheet{
 
         // use the input to populate the array of Cells
         for (int i=0; i < rows; i++) {
-
+            String rowChar = getStrForNumber(i);
             for (int j=0; j < cols; j++) {
                 String next = in.nextLine();
                 input[cols*i+j] = next; 
                 cells[i][j] = new Cell(next);
+
+                // use cellString to map each Cell to a string for O(1) lookup
+                String cellString = rowChar + String.valueOf(j+1);
+                // System.out.print(cellString);
+                locationMap.put(cellString, cells[i][j]);
             }
         }
 
@@ -64,7 +77,6 @@ public class Spreadsheet{
         for (String s : input) {
             System.out.println(s);
         }
-
         return cells;
     }
 
@@ -74,11 +86,17 @@ public class Spreadsheet{
                System.out.println(cells[i][j]);
     }
 
+    public static HashSet visited = new HashSet();
+
     public static void runMain() {
         Cell[][] cells = readInput();
         for (int i=0; i < cells.length; i++)
-            for (int j=0; j < cells[0].length; j++)
-                break;// eval(cell);
+            for (int j=0; j < cells[0].length; j++) {
+                // reset the hashset first
+                HashSet visited = new HashSet();
+                eval(cells[i][j]);
+                System.out.println(String.format("%.5f", cells[i][j].getVal()));
+            }
     }
 
     // main eval function
@@ -86,30 +104,74 @@ public class Spreadsheet{
     // if its a expression - make a list of its dependencies, and eval those recursively
     // updates a "visited" list of "nodes" (cells) to check for cycles
 
-    public static HashSet visited = new HashSet();
-
+    // in future versions of this project - could optimize the eval calls with
+    // a topological sort of the "graph" of cells so that we minimize repeated evals
     public static void eval(Cell rootCell) {
         if (rootCell.isLiteral())
             return;
-        else {
-            // reset the hashset first
-            HashSet visited = new HashSet();
+            //return cell.getVal();
+        else if (rootCell.getDependencies().size() > 0) {
+            if (visited.contains(rootCell)) {
+                System.out.println("CYCLE FOUND TODO");
+            }
+            visited.add(rootCell);
             // go through the arraylist of dependencies, and use the helper eval on each
             for (String s: rootCell.getDependencies()) {
                 // convert string to actual dependent node
                 // then call helper eval on that
-                break;
+                Cell dependent = locationMap.get(s);
+                eval(dependent);
             }
-           
         }
-         
+        else {
+            // evaluate some math expression then
+            double result = evalHelper(rootCell.getStrExpr());
+            rootCell.setVal(result);
+            rootCell.setLiteral(true);
+        }
     }
 
-    public static void main(String[] args) {
-        testCellConstructor();
-        Cell[][] cells = readInput();
-        debugDisplayCells(cells);
+    // this function takes some math expression and "solves it"
+    // assume we have the information we need (i.e. no dependencies)
+    public static double evalHelper(String expression) {
 
-        System.out.println(getCharForNumber(0));
+        // initialize Stack
+        // for each element, if its a number, add it to the stack
+        // if its an operator? then pop the last two and perform the operator
+        // keep going until stack empty and we have reached the end of the expression 
+        
+        Stack tokenStack = new Stack();
+        for (String word : expression.split(" ")) {
+            if (isOperator(word)) {
+                double op2 = (double)tokenStack.pop(); 
+                double op1 = (double)tokenStack.pop();
+                double result = Integer.MAX_VALUE; // some default
+                if (word == "+")
+                    result = op1 + op2;
+                else if (word == "-")
+                    result = op1 - op2;
+                else if (word == "*")
+                    result = op1 * op2;
+                else if (word == "/")
+                    result = op1 / op2;
+                tokenStack.push(new Double(result));
+            }
+            else 
+                System.out.println("word here is " + word);
+                tokenStack.push(Double.parseDouble(word));
+        }
+        if (tokenStack.size() == 1) 
+            return (double)tokenStack.pop();
+        // if we are here, then we had a bad expression...
+        System.out.println("stack problem TODO");
+        throw new IllegalArgumentException("bad row/col input"); 
+    }
+
+
+    public static void main(String[] args) {
+        // testCellConstructor();
+        // Cell[][] cells = readInput();
+        // debugDisplayCells(cells);
+        runMain();
     }
 }
